37fb2c76e39dcf5b9bd288fc926b9bbcee2369d0
jiangege <jiangege47@gmail.com>
2016-04-03 17:06:25 +0800

    0.9

--- src/js/bus.js
+++ src/js/bus.js
@@ -16,11 +16,7 @@ Bus = {
       url: this.url + path,
       method: method,
       type: 'text',
-      cache: false,
-      headers: {
-        "User-Agent": "Dalvik/2.1.0 (Linux; U; Android 5.1; m2 note Build/LMY47D)",
-        "Host": "api.chelaile.net.cn:7000"
-      }
+      cache: false
     };
     if (method === "get") {
       opts.url += this.preFillParams(params);
